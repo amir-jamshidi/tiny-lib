3086ec2fce56295453895d5b008c497b0ef3aaa5
Amir Jamshidi <aabbccddee9898@gmail.com>
2025-12-26 22:27:43 +0330

    feat: implement some files

--- release.config.cjs
+++ release.config.cjs
@@ -1,31 +1,40 @@
 module.exports = {
-    branches: [
-        "main"
-    ],
+    branches: ["main"],
     repositoryUrl: "https://github.com/amir-jamshidi/tiny-lib.git",
+  
     plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        [
-            "@semantic-release/changelog",
-            {
-                changelogFile: "CHANGELOG.md"
-            }
-        ],
-        [
-            "@semantic-release/npm",
-            {
-                npmPublish: true
-            }
-        ],
-        "@semantic-release/github",
-        [
-            "@semantic-release/git",
-            {
-                assets: ["dist", "CHANGELOG.md", "package.json"],
-                message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-            }
-        ]
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+  
+      [
+        "@semantic-release/exec",
+        {
+          prepareCmd: "npm run build"
+        }
+      ],
+  
+      [
+        "@semantic-release/changelog",
+        {
+          changelogFile: "CHANGELOG.md"
+        }
+      ],
+  
+      [
+        "@semantic-release/npm",
+        {
+          npmPublish: true
+        }
+      ],
+  
+      [
+        "@semantic-release/git",
+        {
+          assets: ["dist", "CHANGELOG.md", "package.json"],
+          message:
+            "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }
+      ]
     ]
-
-}
+  };
+  
